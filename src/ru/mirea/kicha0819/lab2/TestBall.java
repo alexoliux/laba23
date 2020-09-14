2ab9d09b8c22e6bb3fd406747932d0fe3b92098f
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2020-09-14 14:32:15 +0300

    Initial commit

---
+++ src/ru/mirea/kicha0819/lab2/TestBall.java
@@ -0,0 +1,12 @@
+package ru.mirea.kicha0819.lab2;
+
+import ru.mirea.kicha0819.lab2.ball;
+
+public class TestBall {
+    public static void main (String[] args) {
+        ball b1 = new ball(50, 50);
+        System.out.println(b1);
+        b1.move(25, 10);
+        System.out.println(b1);
+    }
+}
